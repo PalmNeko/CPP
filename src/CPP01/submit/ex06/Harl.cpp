7e86a7137a494ee3f317fa87504727c5d9b315b9
PalmNeko <nitunamoni13579@gmail.com>
2025-05-04 07:28:49 +0900

    update: Harl

--- src/CPP01/submit/ex06/Harl.cpp
+++ src/CPP01/submit/ex06/Harl.cpp
@@ -3,96 +3,96 @@
 #include <iostream>
 
 /*
- * special member functions
- */
-Harl::Harl(void)
-{}
-
-Harl::Harl(const Harl& harl)
-{
-    (void)harl;
-}
-
-Harl::~Harl(void)
-{}
-
-/*
- * operators
- */
-Harl& Harl::operator=(const Harl& harl)
-{
-    if (this != &harl)
-    {
-    }
-    return *this;
-}
-
-/*
  * others: public
  */
 
-void Harl::debug(void)
-{
-    std::cout << "I love having extra bacon for my"
-        " 7XL-double-cheese-triple-pickle-special ketchup burger. " << std::endl;
-    std::cout << "I really do!" << std::endl;
-}
-
-void Harl::info(void)
-{
-    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
-    std::cout << "You didn’t put enough bacon in my burger!" << std::endl;
-    std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
-}
-
-void Harl::warning(void)
-{
-    std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
-    std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
-}
-
-void Harl::error(void)
-{
-    std::cout <<
-        "This is unacceptable, "
-        "I want to speak to the manager now." << std::endl;
-}
-
-void Harl::insignificant(void)
-{
-    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-}
-
 void Harl::complain(std::string level)
 {
+    enum {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR,
+        NONE_LOG_LEVEL,
+    };
     struct LevelMap {
         std::string level;
-        void (Harl::*claim)(void);
+        int value;
     };
-    // claim2 = &Harl::debug;
     LevelMap map[] = {
-        {"DEBUG", &Harl::debug},
-        {"INFO", &Harl::info},
-        {"WARNING", &Harl::warning},
-        {"ERROR", &Harl::error},
+        {"DEBUG", DEBUG},
+        {"INFO", INFO},
+        {"WARNING", WARNING},
+        {"ERROR", ERROR},
     };
+    
     int mapSize = sizeof(map) / sizeof(map[0]);
-    bool harlSwitch = false;
+    int levelValue = NONE_LOG_LEVEL;
     for (int i = 0; i < mapSize; i++)
     {
-        if (map[i].level == level || harlSwitch)
+        if (map[i].level == level)
         {
-            std::cout << "[ " + map[i].level + " ]" << std::endl;
-            (this->*(map[i].claim))();
-            std::cout << std::endl;
-            harlSwitch = true;
+            levelValue = map[i].value;
+            break ;
         }
     }
-    if (harlSwitch == false)
-        this->insignificant();
+    switch (levelValue)
+    {
+        case DEBUG:
+            this->debug();
+        case INFO:
+            this->info();
+        case WARNING:
+            this->warning();
+        case ERROR:
+            this->error();
+        break ;
+        default:
+            this->insignificant();
+    }
     return ;
 }
 
 /*
  * others: private
  */
+
+void Harl::debug(void) const
+{
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my"
+        " 7XL-double-cheese-triple-pickle-special ketchup burger. " << std::endl;
+    std::cout << "I really do!" << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::info(void) const
+{
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+    std::cout << "You didn’t put enough bacon in my burger!" << std::endl;
+    std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::warning(void) const
+{
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+    std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::error(void) const
+{
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout <<
+        "This is unacceptable, "
+        "I want to speak to the manager now." << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::insignificant(void) const
+{
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
--- src/CPP01/submit/ex06/Harl.hpp
+++ src/CPP01/submit/ex06/Harl.hpp
@@ -5,25 +5,14 @@
 
 class Harl
 {
-    // variables
     public:
-    private:
-    // functions
-    public:
-        // special member functions
-        Harl(void); // default constructor
-        Harl(const Harl& harl); // copy constructor
-        ~Harl(void); // destructor
-        // operators
-        Harl& operator=(const Harl& harl); // operator=
-        // others
         void complain(std::string level);
     private:
-        void debug(void);
-        void info(void);
-        void warning(void);
-        void error(void);
-        void insignificant(void);
+        void debug(void) const;
+        void info(void) const;
+        void warning(void) const;
+        void error(void) const;
+        void insignificant(void) const;
 };
 
 #endif
