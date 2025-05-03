dd7ece82c47e7985491189ddb40b1e03b978f1c1
PalmNeko <nitunamoni13579@gmail.com>
2025-05-04 07:09:03 +0900

    update: add title

--- src/CPP01/submit/ex05/Harl.cpp
+++ src/CPP01/submit/ex05/Harl.cpp
@@ -30,7 +30,7 @@ void Harl::complain(std::string level) const
 void Harl::warning(void) const
 {
 	std::cout <<
-		"I think I deserve to have some extra bacon for free."
+		"[WARNING] I think I deserve to have some extra bacon for free."
 		" I’ve been coming for years whereas you started working here"
 		" since last month." << std::endl;
 }
@@ -38,7 +38,7 @@ void Harl::warning(void) const
 void Harl::info(void) const
 {
 	std::cout <<
-		"I cannot believe adding extra bacon costs more money."
+		"[INFO] I cannot believe adding extra bacon costs more money."
 		" You didn’t put enough bacon in my burger!"
 		" If you did, I wouldn’t be asking for more!" << std::endl;
 }
@@ -46,14 +46,14 @@ void Harl::info(void) const
 void Harl::error(void) const
 {
 	std::cout <<
-		"This is unacceptable!"
+		"[ERROR] This is unacceptable!"
 		" I want to speak to the manager now." << std::endl;
 }
 
 void Harl::debug(void) const
 {
 	std::cout <<
-		"I love having extra bacon for my"
+		"[DEBUG] I love having extra bacon for my"
 		" 7XL-double-cheese-triple-pickle-special ketchup burger. "
 		"I really do!" << std::endl;
 }
