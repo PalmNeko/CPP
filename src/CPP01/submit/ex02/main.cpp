00b3c357ca8ca6fd4b708fbd392f740cb4d18957
PalmNeko <nitunamoni13579@gmail.com>
2025-05-03 20:44:59 +0900

    update: delete dasoku

--- src/CPP01/submit/ex02/main.cpp
+++ src/CPP01/submit/ex02/main.cpp
@@ -5,12 +5,11 @@ int main(void)
     std::string brain("HI THIS IS BRAIN");
     std::string *stringPTR = &brain;
     std::string &stringREF = brain;
-    std::cout << stringPTR << std::endl;
-    std::cout << "brain pointer: " << &brain << std::endl;
-    std::cout << "stringPTR: " << stringPTR << std::endl;
-    std::cout << "stringREF: " << &stringREF << std::endl;
+    std::cout << "brain address: " << &brain << std::endl;
+    std::cout << "stringPTR value: " << stringPTR << std::endl;
+    std::cout << "stringREF address: " << &stringREF << std::endl;
     std::cout << "brain value: " << brain << std::endl;
-    std::cout << "*stringPTR: " << *stringPTR << std::endl;
-    std::cout << "stringREF: " << stringREF << std::endl;
+    std::cout << "*stringPTR value: " << *stringPTR << std::endl;
+    std::cout << "stringREF value: " << stringREF << std::endl;
     return (0);
 }
