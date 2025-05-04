b9547aaf9372028a558630b53497743dcc14ab0f
PalmNeko <nitunamoni13579@gmail.com>
2025-05-04 16:49:57 +0900

    update: previous files

--- src/CPP03/submit/ex02/ClapTrap.hpp
+++ src/CPP03/submit/ex02/ClapTrap.hpp
@@ -6,13 +6,11 @@
 class ClapTrap
 {
     // variables
-    public:
-    protected:
+    private:
         std::string name;
         unsigned int hitPoints;
         unsigned int energyPoints;
         unsigned int attackDamage;
-    private:
     // functions
     public:
         // special member functions
@@ -24,6 +22,14 @@ class ClapTrap
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
+        const std::string& getName(void) const;
+        unsigned int getHitPoints(void) const;
+        unsigned int getEnergyPoints(void) const;
+        unsigned int getAttackDamage(void) const;
+        void setName(const std::string name);
+        void setHitPoints(unsigned int hitPoints);
+        void setEnergyPoints(unsigned int energyPoints);
+        void setAttackDamage(unsigned int attackDamage);
         // operators
         ClapTrap& operator=(const ClapTrap& clapTrap); // copy assignment operator
     private:
