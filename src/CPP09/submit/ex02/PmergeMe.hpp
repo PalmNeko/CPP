b1d1a9a26c87cc35fa83db1210af08fca733ff1d
PalmNeko <nitunamoni13579@gmail.com>
2025-06-16 05:22:37 +0900

    delete: unused function

--- src/CPP09/submit/ex02/PmergeMe.hpp
+++ src/CPP09/submit/ex02/PmergeMe.hpp
@@ -24,17 +24,11 @@ class PmergeMe
         static Container pmergeme(InputIterator first, InputIterator last);
         static void print(InputIterator first, InputIterator last);
         static Container create_pairs(InputIterator first, InputIterator last, Node **leftovers);
-        static Container create_mainchain(InputIterator first, InputIterator last);
-        static Container create_subchain(InputIterator first, InputIterator last, Node **leftovers);
         static size_t calc_index(size_t no);
         static InputIterator at(Container &container, int index);
-        static void insertAll(Container &mainchain, Container &subchain);
-        static void insert(Container &mainchain, Node *value);
         static void binary_insert(Container &container, Node *value);
-        static Container jacob_merge(Container &sorted_pairs);
         static Container jacob_merge2(Container &sorted_pairs, Node *leftovers);
         static InputIterator binary_insert_iterator(InputIterator first, InputIterator last, const Node &value);
-        static Container flatten(InputIterator first, InputIterator last);
         static void destroy_pairs(InputIterator first, InputIterator end);
 };
 
